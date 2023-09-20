e38efc1cadff416dc7859d039aecc6e3696cf3ef
Juan Esteban Ramirez Perdomo <115906656+Jramirezzz@users.noreply.github.com>
2023-09-20 08:10:47 -0500

    Update index.js

--- index.js
+++ index.js
@@ -6,23 +6,11 @@ const clasesRouter = require('./classes');
 const principalsRouter = require('./principals')
 
 
-// app.get('/students',(req,res)=>{
-//     res.send("Un monton de profesores")
-// })
-
-// app.get('/classes',(req,res)=>{
-//     res.send("Un monton de profesores")
-// })
-
-// app.get('/teachers',(req,res)=>{
-//     res.send("Un monton de profesores")
-// })
-
-app.use('/students, studentsrouter');
-app.use('/classes, clasesRouter');
-app.use('/principals, principalsRouter ')
+app.use('/students', studentsrouter');
+app.use('/classes', clasesRouter');
+app.use('/principals', principalsRouter ')
 
 app.listen(3000, ()=>{
     console.log()
-    res.send
+    res.send("start tmfs")
 })
